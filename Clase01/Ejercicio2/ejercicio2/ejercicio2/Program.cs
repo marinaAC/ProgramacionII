ba815f5efe5515bc60d231439e82a1d7daea7892
marinaAC <maru.cardozo02@gmail.com>
2017-08-14 21:54:44 -0300

    Primera clase

---
+++ Clase01/Ejercicio2/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+            string numeroIngresado;
+            Console.WriteLine("Ingrese un numero: ");
+            numeroIngresado = Console.ReadLine();
+            numero = float.Parse(numeroIngresado);
+            if (numero > 0)
+            {
+                numero = (long)Math.Pow(numero, 2);
+                Console.WriteLine("El numero al cuadrado es: {0:N}", numero);
+                Console.ReadLine();
+            }
+            else {
+                Console.WriteLine("ERROR EL NUMERO TIENE QUE SER MENOR QUE 0!!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
