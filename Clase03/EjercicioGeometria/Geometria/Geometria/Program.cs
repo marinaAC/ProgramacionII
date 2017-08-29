36299c5f6af3fea66d8cdb8c70bdf32d0dc024a6
marinaAC <maru.cardozo02@gmail.com>
2017-08-28 22:07:46 -0300

    Ejercicio Geometria terminado

---
+++ Clase03/EjercicioGeometria/Geometria/Geometria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometria;
+
+namespace PruebaGeometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valorIngresado;
+            int valor1;
+            int valor2;
+            int valor3;
+            int valor4;
+
+            Console.WriteLine("Ingrese el valor de x: ");
+            valorIngresado = Console.ReadLine();
+            valor1 = int.Parse(valorIngresado);
+
+            Console.WriteLine("Ingrese el valor de x: ");
+            while(!int.TryParse(Console.ReadLine(),out valor2)){
+                Console.WriteLine("Error en el parametro");
+            }
+
+            Console.WriteLine("Ingrese el valor de y: ");
+            while (!int.TryParse(Console.ReadLine(), out valor3))
+            {
+                Console.WriteLine("Error en el parametro");
+            }
+
+            Console.WriteLine("Ingrese el valor de y: ");
+            while (!int.TryParse(Console.ReadLine(), out valor4))
+            {
+                Console.WriteLine("Error en el parametro");
+            }
+
+            Rectangulo rectangulo = new Rectangulo(new Punto(valor1, valor3), new Punto(valor2, valor4));
+
+            Console.WriteLine("Area: {0:N}", rectangulo.area);
+            Console.WriteLine("Perimetro: {0:N}", rectangulo.perimetro);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Clase03/EjercicioGeometria/Geometria/Geometria/Punto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Punto
+    {
+        private int _x;
+        private int _y;
+
+        public Punto(int x, int y) {
+            this._x = x;
+            this._y = y;
+        }
+
+        public int getX() {
+            return this._x;
+        }
+
+        public int getY() {
+            return this._y;
+        }
+
+    }
+}
