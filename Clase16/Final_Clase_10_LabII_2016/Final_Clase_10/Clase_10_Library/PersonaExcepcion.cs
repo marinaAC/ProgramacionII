9c71cd0051c89f3fd60ecb5a0545b6f7e50c5349
marinaAC <maru.cardozo02@gmail.com>
2017-10-09 21:51:49 -0300

    clase del dia

---
+++ Clase16/Final_Clase_10_LabII_2016/Final_Clase_10/Clase_10_Library/PersonaExcepcion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_8_Library;
+
+
+namespace Clase_10_Library
+{
+    public class PersonaExcepcion: Exception
+    {
+        public PersonaExcepcion(string men)
+            : base(men)
+       {
+       }
+    }
+}
---
+++ Clase16/Final_Clase_10_LabII_2016/Final_Clase_10/TestClase10/PorcionAccionariaTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clase_10_Library;
+using Clase_8_Library;
+
+namespace TestClase10
+{
+    [TestClass]
+    public class PorcionAccionariaTest
+    {
+        [TestMethod]
+        public void PorcionAccionariaTest()
+        {
+            Accionista accCorrecta = new Accionista("Pedro","Alberto", 20);
+            Assert.AreEqual(accCorrecta.PorcionAccionaria, 20);
+
+            Accionista accCorrecta2 = new Accionista("Pedro", "Alberto", 50);
+            Assert.AreEqual(accCorrecta2.PorcionAccionaria, 50);
+
+
+            try
+            {
+                Accionista fueraRango = new Accionista("Pedro", "Alberto", -1);
+            }
+            catch (AccionMenorCeroEx e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+
+
+            try
+            {
+                Accionista fueraRango = new Accionista("Pedro", "Alberto", 10000);
+            }
+            catch (AccionMayorACienEx e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
