954a6350fec3f5b5c9033e6fdde60f6847835d46
marinaAC <maru.cardozo02@gmail.com>
2017-10-17 20:08:55 -0300

    clase del dia

---
+++ Clase19/ejercicio55/ejercicio55/ejercicio55/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio55
+{
+    public partial class formPrincipal : Form
+    {
+        public formPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openfile.FilterIndex = 1;
+
+
+            openfile.Multiselect = true;
+
+           
+            if(openfile.ShowDialog() == DialogResult.OK)
+            {
+                
+            }
+        }
+    }
+}
