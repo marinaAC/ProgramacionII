e514b53250dd8c0c796884bef5c0b1ef3a08c12d
marinaAC <maru.cardozo02@gmail.com>
2017-10-30 22:07:39 -0300

    Base de datos, practicas de clase

---
+++ Clase21/ejercioEnClase/baseDeDatos/baseDeDatos/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace baseDeDatos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbProvincia = new System.Windows.Forms.ComboBox();
+            this.cmbLocalidad = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbProvincia
+            // 
+            this.cmbProvincia.FormattingEnabled = true;
+            this.cmbProvincia.Location = new System.Drawing.Point(12, 44);
+            this.cmbProvincia.Name = "cmbProvincia";
+            this.cmbProvincia.Size = new System.Drawing.Size(255, 21);
+            this.cmbProvincia.TabIndex = 0;
+            this.cmbProvincia.SelectedIndexChanged += new System.EventHandler(this.cmbProvincia_SelectedIndexChanged);
+            // 
+            // cmbLocalidad
+            // 
+            this.cmbLocalidad.FormattingEnabled = true;
+            this.cmbLocalidad.Location = new System.Drawing.Point(13, 144);
+            this.cmbLocalidad.Name = "cmbLocalidad";
+            this.cmbLocalidad.Size = new System.Drawing.Size(254, 21);
+            this.cmbLocalidad.TabIndex = 1;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.cmbLocalidad);
+            this.Controls.Add(this.cmbProvincia);
+            this.Name = "frmPrincipal";
+            this.Text = "Base de datos";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbProvincia;
+        private System.Windows.Forms.ComboBox cmbLocalidad;
+    }
+}
+
---
+++ Clase21/ejercioEnClase/baseDeDatos/baseDeDatos/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace baseDeDatos
+{
+    public partial class frmPrincipal : Form
+    {
+        SqlConnection conexion;
+        string connectionStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=Argentina;Integrated Security=True";
+        SqlCommand select;
+        /// <summary>
+        /// Profesor declara aca dentro el sqlconnection
+        /// </summary>
+        public frmPrincipal()
+        {
+            InitializeComponent();
+
+        }
+
+        private void cmbProvincia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            conexion = new SqlConnection(connectionStr);
+            select = new SqlCommand("Select l.Nombre from Localidad l inner join Provincia p on l.idProvincia = p.id where p.descripcion = '"+this.cmbProvincia.Text+"'",conexion);
+            try
+            {
+                conexion.Open();
+                SqlDataReader dr = select.ExecuteReader();
+                this.cmbLocalidad.Items.Clear();
+                while (dr.Read())
+                {
+                    this.cmbLocalidad.Items.Add(dr[0]);
+                }
+            }
+            catch (Exception a)
+            {
+
+                MessageBox.Show(a.Message);
+            }
+            finally 
+            {
+                conexion.Close();
+            }
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs n)
+        {
+            conexion = new SqlConnection(connectionStr);
+            select = new SqlCommand("Select * from Provincia");
+            try
+            {
+                conexion.Open();
+                select.Connection = conexion;
+                SqlDataReader oDr = select.ExecuteReader();
+                cmbProvincia.Items.Clear();
+                while (oDr.Read())
+                {
+                    this.cmbProvincia.Items.Add(oDr["descripcion"].ToString());
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message + "Error no pudo abrirse la base de datos");
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+
+        /*
+        private SqlDataReader selectFromBase()
+        {
+            select = new SqlCommand("Select * from Provincias");
+            //select.CommandType = System.Data.CommandType.Text;
+            //select.Connection = conexion;
+            //select.CommandText = "Select * from Provincias";
+            conexion.Open();
+            SqlDataReader oDr = select.ExecuteReader();
+            return oDr;
+        }*/
+    }
+}
