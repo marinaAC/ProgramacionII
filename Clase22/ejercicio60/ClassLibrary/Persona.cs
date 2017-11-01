0a3538da7b1b0e320b3d6534200478845f0b11d1
marinaAC <maru.cardozo02@gmail.com>
2017-10-31 22:06:56 -0300

    parte de lo visto en clase

---
+++ Clase22/ejercicio60/ClassLibrary/Persona.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Persona
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+
+        public int Id 
+        {
+            get 
+            {
+                return id;
+            }
+        }
+
+        public string Nombre 
+        { 
+            get
+            {
+                return nombre;
+            }
+            set 
+            {
+                nombre = value;
+            } 
+        }
+
+        public string Apellido 
+        {
+            get 
+            {
+                return apellido;
+            }
+            set 
+            {
+                apellido = value;
+            } 
+        }
+
+        public Persona(string id,string nombre, string apellido) 
+        {
+
+            this.apellido = apellido;
+            this.nombre = nombre;
+            if (!int.TryParse(id, out this.id)) 
+            {
+                throw new Exception();
+            }
+        }
+
+
+    }
+}
---
+++ Clase22/ejercicio60/ClassLibrary/PersonaDAO.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class PersonaDAO
+    {
+
+        public static bool Guardar(SqlConnection conexion, Persona p) 
+        {
+            bool returnAux = false;
+            string scriptInsert = string.Format("INSERT INTO Persona (Nombre,Apellido) values ('{0}','{1}')", p.Nombre,p.Apellido);
+            SqlCommand insertPersona = new SqlCommand(scriptInsert);
+            try
+            {
+                conexion.Open();
+                insertPersona.ExecuteNonQuery();
+                returnAux = true;
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally 
+            {
+                if(conexion.State == System.Data.ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+
+            return returnAux;
+        }
+
+        
+        public static bool Leer(SqlConnection conexion, List<Persona> personas) 
+        {
+            bool returnAux = false;
+            string s = string.Format("select * from Persona");
+            SqlCommand cargarList = new SqlCommand(s);
+            if(personas.Count<0)
+            {
+                conexion.Open();
+                SqlDataReader basePersona = cargarList.ExecuteReader();
+                while (basePersona.Read())
+                {
+                    string id = basePersona["Id"].ToString();
+                    string nombre = basePersona["Nombre"].ToString();
+                    string apellido = basePersona["Apellido"].ToString();
+                    Persona p = new Persona(id,nombre,apellido);
+                    personas.Add(p);
+                    
+                }
+                returnAux = true;
+            }
+
+
+            try
+            {
+                conexion.Open();
+
+
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+             
+        }
+        public static bool LeerPorId(SqlConnection conexion, Persona p) 
+        {
+            bool returnAux = false;
+            string s = string.Format("select * from Persona p where p.id = {0}", p.Id);
+            SqlCommand selectId = new SqlCommand(s);
+            try
+            {
+                conexion.Open();
+                SqlDataReader basePersona = selectId.ExecuteReader();
+                while (basePersona.Read())
+                {
+                        p.Nombre = basePersona["Nombre"].ToString();
+                        p.Apellido = basePersona["Apellido"].ToString();
+                        returnAux = true;
+                }
+          
+            }
+            catch (Exception)
+            {
+
+                throw new Exception();
+            }
+
+            return returnAux;
+        }
+        /*
+        public static bool Modificar() { }
+        public static bool Borrar() { }*/
+    }
+}
